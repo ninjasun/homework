c6c0af2e1e789502773c3f6f063fb4e7220e4f17
Bilal Quadri <bilalquadri92@gmail.com>
2012-10-10 00:57:47 -0400

    Structure

---
+++ 214/pa3/indexer.c
@@ -0,0 +1,12 @@
+#include "indexer.h"
+
+int main(int argc, char **argv) {
+  puts("Hello world!");
+
+  if (argc != 3) {
+    printf("Invalid input. Two arguments are required\n");
+    return 1;
+  }
+
+  return 0;
+}
