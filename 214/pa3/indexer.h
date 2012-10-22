fe7b6be27971e0faf1a3d9a353f82b09d02dbe40
Bilal Quadri <bilalquadri92@gmail.com>
2012-10-21 20:28:31 -0400

    Tokenization complete

--- 214/pa3/indexer.h
+++ 214/pa3/indexer.h
@@ -1,14 +1,41 @@
 #ifndef INDEXER_H
 #define INDEXER_H
 
+/* Globals Variables */
+char* delims;
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <dirent.h>
 #include <sys/stat.h>
+#include <ftw.h>
+#include <ctype.h>
 
 
+/* Structs */
+struct fileNode {
+  char* fileName;
+  int count;
+  struct fileNode* next;
+};
+
+struct wordNode{
+  char* word;
+  struct fileNode* files;
+  struct wordNode* next;
+};
+
+
+/* Global Structs */
+struct wordNode* wordList;
+
+/* Functions */
 int fileCallback(const char* pathname, const struct stat* ptr, int flag);
+char* getDelims();
+int addCount(char* path, char* word);
+void freeFiles();
+void freeWords();
 
 
 #endif
