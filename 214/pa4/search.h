9ad358001e89da397fcfd662bbe0ba53555d6c9e
Bilal Quadri <bilalquadri92@gmail.com>
2012-11-11 19:34:18 -0500

    Added header for a func

--- 214/pa4/search.h
+++ 214/pa4/search.h
@@ -5,6 +5,17 @@
 #include <stdio.h>
 #include <string.h>
 
+typedef struct filenode {
+  char* word;
+  fileNode* next;
+} fileNode;
+
+typedef struct wordnode {
+  char* word;
+  wordNode* next;
+} wordNode;
+
 char** buildFiles(const char*);
+file** buildWordList(const char*);
 
 #endif
