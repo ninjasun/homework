1349ed00f330ce86ad250df27f5978ce08a76828
ace6598 <ace6598@gmail.com>
2012-12-12 19:47:55 -0500

    gundamFreedom

--- 214/pa6/books.c
+++ 214/pa6/books.c
@@ -162,4 +162,37 @@ int main(int argc, char** argv) {
   return 0;
 }
 
+//customerlist (contains 2 linked lists of hist and a shit ton of strings), orders(strings)
+void gundamFreedom() {
+  struct book* tmp;
+  while (orders != NULL) {
+    tmp = orders;
+    orders = orders->next;
+    free(tmp->title);
+    free(tmp->category);
+    free(tmp);
+  }
 
+  struct customer* cusTmp;
+  while (customerList != NULL) {
+    tmp = customerList;
+    customerList = customerList->next;
+    while(tmp->success != NULL) {
+      struct hisTmp = tmp->success;
+      tmp->success = tmp->success->next;
+      free(hisTmp->line);
+      free(hisTmp);
+    }
+    while(tmp->fail != NULL) {
+      struct hisTmp = tmp->fail;
+      tmp->fail = tmp->fail->next;
+      free(hisTmp->line);
+      free(hisTmp);
+    }
+    free(tmp->name);
+    free(tmp->address);
+    free(tmp->state);
+    free(tmp->zip);
+    free(tmp);
+  }
+}
